4ee0d87be017854b07e47608fbbc9ddbeefed90b
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2020-10-21 09:13:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,169 @@
+# FAROS: A Framework for Benchmarking and Analysis of Compiler Optimization
+
+## Description and usage
+
+This repo contains a benchmark harness to fetch, build, and run programs
+with different compilation options for analyzing the impact of
+compilation on performance. The usecase is contrasting OpenMP compilation
+with its serial elision to understand performance differences due to
+different compilation.
+
+
+### Harness script
+
+The harness script in python, named benchmark.py, takes as input a YAML
+configuration file and a set of options to build and run programs
+described in that configuration. Figure fig:harness shows the help
+output describing possible options. The configuration file input is set
+with the `-i, --input` argument. There are three different actions the
+harness performs:
+1. fetch sources, with the option `-f, --fetch`,
+fetches the program sources from the specified repositories;
+2. build programs, with
+the option `-b, --build`, builds the selected program using the specified
+compilation options in the configuration, also fetching if needed;
+3. generate reports, with the option `-g, --generate`, generates compilation reports
+by combining optimization remarks for different compilation
+configurations, creating remark diff files between them, from all the
+sources of an application to a single file;
+4. run tests, with the option `-r,--run` and a following argument on how many repetitions to perform, that
+runs the executable with the specified input, repeating up to the number
+of repetitions set.
+
+The flags can be individually set doing multiple runs of the harness, or
+combined to perform multiple actions in a single run -- fetching takes
+precedence over building, building over generating reports and running.
+The user select the list of programs to operate usin `-p,--programs`
+followed by individual program names or the keyword `all` for all the
+programs specified in the configuration input.  Alternatively, the user
+may select the list of programs by provide a tag list with `-t,--tags`
+that matches tags for each program specified in the configuration.
+Also, the harness has a dry run option, `-d,--dry-run`, that prints what
+actions would be performed without actually performing them.
+
+Below is the output when running help, `-h, --help` on the harness
+script:
+```
+Benchmark and analyze programs compiled with different compilation options.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        configuration YAML input file for programs
+  -f, --fetch           fetch program repos (without building)
+  -b, --build           build programs (will fetch too)
+  -r RUN, --run RUN     run <repetitions>
+  -g, --generate        generate compilation reports
+  -p PROGRAMS [PROGRAMS ...], --programs PROGRAMS [PROGRAMS ...]
+                        programs to run from the config
+  -t TAGS [TAGS ...], --tags TAGS [TAGS ...]
+                        tagged program to use from the config
+  -s, --stats           show run statistics
+  -d, --dry-run         enable dry run
+```
+The harness creates four extra directories for its
+operation when building and running:
+1. the directory `repos` to download the benchmark application
+    specified in the configuration;
+2. the directory `bin` to store the generated executable from
+    building to run them;
+3. the directory `reports`, where it store compilation
+    reports, including optimization remarks;
+4. the directory `results` to store profiling results, which
+    contain execution times from running different built configurations and
+    inputs.
+
+### YAML configuration input
+
+Configuring YAML creates is a hierarchy of keys for each program to
+include that prescribe actions for the harness script. We describe those
+keys here. For a working example please see `config.yaml` in the repo,
+which iincludes configuration for 39 HPC programs including proxy/mini
+applications, NAS and Rodinia kernels, and the open-source large
+application GROMACS.  The root of the hierarchy is a user-chosen,
+descriptive name per program configuration.  The harness creates a
+sub-directory matching the name of this root key under `bin` to store
+executables
+
+The key `fetch` contains the shell command to fetch the
+application code, for example cloning from a GitHub repo. Note that the
+fetching command can include also a patching file, if needed, provided
+by the user. In this repo we provide patch files for programs in
+`config.yaml` undr the directory `patches`.  For example, for some
+programs, we apply a patch to guard calls to OpenMP runtime functions
+using the standard approach of enabling those calls within `#ifdef OPENMP
+... endif` preprocessor directives.
+
+The key `tags` sets a list of user-defined tags which can be used by the
+harness to include programs when performing its operation.
+
+The key `build_dir` specifies the directory to build the application, so
+harness changes to this directory to execute the build commands
+specified under the key build. There is a different sub-key for each
+different compilation specification, denoted by a user-provided
+identifier.  The harness creates different sub-directories under
+`bin/<program>`for each different compilation configurations
+
+The key `copy` specifies a list of files or directories that the harness
+copies out to those sub-directories. The list contains the executable
+file and possibly any input files needed for executing, if the user
+desires to have self-contained execution in `bin` by avoiding referring to
+input files in the directory repos -- this is useful for
+relocating the directory bins without needing to copy over
+repos.
+
+Further, the key `run` specifies the command to
+execute, which is typically the executable binary of the application,
+prepended with any environment variables to set.
+
+Moreover, the key `input` specifies the input arguments
+for the application in the run command.
+
+The key `measure`
+specifies a regular expression to match in the application's executable
+output to capture the desired measure of performance, such as execution
+time or some other Figure of Merit (FoM). If the value of the key
+measure is empty, the harness measures end-to-end, wall clock  execution
+time from launching the application to its end, using python's
+time module.
+
+Lastly, the key `clean` specifies the commands that harness executes to
+clean the repo for building a different compilation configuration.
+
+## Contributing
+To contribute to this repo please send a [pull
+request](https://help.github.com/articles/using-pull-requests/) on the
+develop branch of this repo.
+
+## Authors
+
+This code was created by Giorgis Georgakoudis (LLNL),
+georgakoudis1@llnl.gov, assisted with technical design input from
+Ignacio Laguna (LLNL), Tom Scogland, and Johannes Doerfert (ANL).
+
+### Citing FAROS
+
+Please cite the following paper: 
+
+* Georgakoudis G., Doerfert J., Laguna I., Scogland T.R.W. (2020) [FAROS: A
+  Framework to Analyze OpenMP Compilation Through Benchmarking and Compiler
+  Optimization
+  Analysis](https://link.springer.com/chapter/10.1007/978-3-030-58144-2_1). In: Milfeld K., de Supinski B., Koesterke L.,
+  Klinkenberg J. (eds) OpenMP: Portable Multi-Level Parallelism on Modern
+  Systems. IWOMP 2020. Lecture Notes in Computer Science, vol 12295. Springer,
+  Cham. https://doi.org/10.1007/978-3-030-58144-2_1
+
+## License
+
+This repo is distributed under the terms of the Apache License (Version
+2.0) with LLVM exceptions. Other software that is part of this
+repository may be under a different license, documented by the file
+LICENSE in its sub-directory.
+
+All new contributions to this repo must be under the Apache License (Version 2.0) with LLVM exceptions.
+
+See files [LICENSE](LICENSE) and [NOTICE](NOTICE) for more information.
+
+SPDX License Identifier: "Apache-2.0 WITH LLVM-exception"
+
+LLNL-CODE-813267
---
+++ harness.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020 Lawrence Livermore National Security, LLC
+# and project developers
+#
+# LLNL release number LLNL-CODE-XXXXXX
+#
+# Author: Giorgis Georgakoudis, georgakoudis1@lln.gov
+#
+# SPDX-License-Identifier: "Apache-2.0 WITH LLVM-exception"
+#
+# See top-level files LICENSE and NOTICE for details.
+#
+
+import argparse
+import yaml
+from yaml import CLoader
+import argparse
+import subprocess
+import os
+import shutil
+import time
+import re
+import sys
+import numpy as np
+from pathlib import Path
+import itertools
+import math
+
+def run(config, program, reps, dry):
+    print('Launching program', program, 'with modes', config[program]['build'])
+    exe = config[program]['run'] + ' ' + config[program]['input']
+    os.makedirs( './results', exist_ok=True)
+    results = { program: {} }
+    try:
+        with open('./results/results-%s.yaml'%(program), 'r') as f:
+            results = yaml.load(f, Loader=CLoader)
+    except FileNotFoundError as e:
+        pass
+
+    for mode in config[program]['build']:
+        bin_dir = './bin/' + program + '/' + mode + '/'
+        if not os.path.isfile( bin_dir + config[program]['bin'] ):
+            print('ERROR: Missing binary %s'%( bin_dir + config[program]['bin'] ) )
+            return
+        try:
+            if (not mode in results[program]) or (not results[program][mode]):
+                start = 0
+                results[program][mode] = []
+            elif len(results[program][mode]) < reps:
+                start = len(results[program][mode])
+            else:
+                print('FOUND', program, mode, 'runs', len(results[program][mode]) )
+                continue
+        except Exception as e:
+            print('ERROR', e, 'running', program, 'mode', mode)
+            sys.exit(1)
+
+        print('RUN', program, mode, 'from', start, 'to', reps)
+
+        if not dry:
+            for i in range(start, reps):
+                print('path', bin_dir, 'exe',exe)
+                t1 = time.perf_counter()
+                p = subprocess.run( exe, capture_output=True, cwd=bin_dir, shell=True )
+                out = str(p.stdout)
+                err = str(p.stderr)
+                output = out + err
+                print('Out', p.stdout.decode('utf-8') )
+                print('Err', p.stderr.decode('utf-8') )
+                with open('%s/stdout-%d.txt'%(bin_dir, i), 'w') as f:
+                    f.write(p.stdout.decode('utf-8'));
+                with open('%s/stderr-%d.txt'%(bin_dir, i), 'w') as f:
+                    f.write(p.stderr.decode('utf-8'));
+
+                if p.returncode != 0:
+                    print('ERROR running', program, 'in', mode)
+                    sys.exit(p.returncode)
+
+                t2 = time.perf_counter()
+
+                print('Time elapsed', t2-t1, ' seconds')
+                print('measure', config[program]['measure'])
+                if config[program]['measure']:
+                    # try stdout
+                    try:
+                        runtime = float( re.search( config[program]['measure'], output).group(1) )
+                        print('runtime:', runtime)
+                    except AttributeError as e:
+                        print('Invalid runtime', e)
+                        runtime = math.inf
+                else:
+                    runtime = t2 - t1
+
+                results[program][mode].append(runtime)
+
+                with open('./results/results-%s.yaml'%(program), 'w') as f:
+                    yaml.dump( results, f )
+
+#def merge_nested_dict(d1, d2):
+#    for k in d1:
+#        if k in d2:
+#            if isinstance(d1[k], dict):
+#                merge_nested_dict(d1[k], d2[k])
+#            else:
+#                if d1[k]:
+#                    d1[k] += d2[k]
+#                else:
+#                    d1[k] = d2[k]
+#            d2.pop(k)
+#    d1.update(d2)
+            
+def show_stats(config, program):
+    try:
+        with open('./results/results-%s.yaml'%(program), 'r') as f:
+            results = yaml.load(f, Loader=CLoader)
+    except FileNotFoundError as e:
+        return
+
+    ranked = sorted( [ (b, np.mean( results[program][b]) ) for b in config[program]['build'] if results[program][b]], key=lambda x: x[1] )
+    no_runs = { b : len( results[program][b])  for b in config[program]['build'] }
+    print('=======')
+    print(program, '# runs', no_runs)
+    print('Results')
+    print('Ranked by speed')
+    print('---------------')
+    for b in ranked:
+        print(b[0], ':', b[1], 's', 'slowdown: %.3f'%(b[1]/ranked[0][1]) )
+    print('=======')
+   
+def merge_stats_reports( program, build_dir, mode ):
+    # generate unified optimization report
+    reports_dir = './reports/' + program + '/'
+    os.makedirs( reports_dir, exist_ok=True )
+
+    # merge reports
+    filenames = Path(build_dir).rglob('*.opt.yaml')
+    data = ''
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            fdata = f.read()
+
+        pathname, basename = os.path.split(filename)
+        #print('pathname', pathname, 'basename', basename)
+        #print('cwd', os.getcwd())
+        # Remove .opt.yaml extension
+        basename = basename.split('.')[0]
+        fdata = fdata.replace( 'File: ' + basename, 'File: ' + os.getcwd() + '/' + pathname + '/' + basename)
+        fdata = re.sub('File: \'([^\.])', 'File: \'' + os.path.expanduser('~') + r'/\1', fdata)
+        fdata = fdata.replace( 'File: \'.', 'File: \'' + os.getcwd() + '/' + pathname + '/.')
+        data += fdata
+
+    with open(reports_dir + mode + '.opt.yaml', 'w') as f:
+        f.write( data )
+
+    # merge stats
+    filenames = Path(build_dir).rglob('*.stats')
+    data = {}
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            print('filename', filename)
+            d = eval( f.read() )
+        data[ str(filename) ] = d
+
+    with open(reports_dir + mode  + '.stats.yaml', 'w') as f:
+        yaml.dump( data, f, default_flow_style=False )
+
+def merge_omp_info( program, build_dir, mode ):
+    # generate unified optimization report
+    reports_dir = './reports/' + program + '/'
+    os.makedirs( reports_dir, exist_ok=True )
+
+    # merge reports
+    filenames = Path(build_dir).rglob('*.omp.yaml')
+    data = {}
+    for filename in filenames:
+        # Remove .opt.yaml extension
+        with open(filename ,'r') as f:
+            d = yaml.load( f, Loader=CLoader )
+        fname = str(filename).split('.omp.yaml')[0]
+        data[ fname ] = d
+
+    with open(reports_dir + program + '.omp.yaml', 'w') as f:
+        yaml.dump( data, f, default_flow_style=False )
+
+def compile_and_install(config, program, repo_dir, mode):
+    build_dir = repo_dir + '/' + config[program]['build_dir']
+    bin_dir = './bin/' + program + '/' + mode + '/'
+    exe = bin_dir + config[program]['bin']
+    # File exists
+    if os.path.isfile( exe ):
+        print('file ' + exe + ' exists')
+        return
+
+    os.makedirs( bin_dir, exist_ok=True )
+    print('Clean...')
+    subprocess.run( config[program]['clean'], cwd=build_dir, shell=True)
+    print('===> Build...program %s mode %s\n%s' % (program, mode, config[program]['build'][mode]) )
+    try:
+        subprocess.run( config[program]['build'][mode], cwd=build_dir, shell=True )
+    except Exception as e:
+        print('building %s mode %s failed'%(program, mode), e)
+        input('key...')
+        sys.exit(1)
+
+    print('Merge stats and reports...')
+    merge_stats_reports( program, build_dir, mode )
+    if mode == 'omp':
+        print('Merge OpenMP info...')
+        merge_omp_info( program, build_dir, mode )
+
+    print('Copy...')
+    for copy in config[program]['copy']:
+        if os.path.isdir( build_dir + '/' + copy ):
+            shutil.copytree( build_dir + '/' + copy, bin_dir + copy)
+        else:
+            shutil.copy( build_dir + '/' + copy, bin_dir)
+
+
+def diff_reports( report_dir, builds, mode ):
+    out_yaml = report_dir + '%s-%s-%s.opt.yaml'%( builds[0], builds[1], mode )
+    out_html = report_dir + 'html-%s-%s-%s'%( builds[0], builds[1], mode )
+    if mode == 'all':
+        opt_diff = './opt-viewer/opt-diff.py '
+    else:
+        opt_diff = './opt-viewer/opt-diff.py --filter %s '%( mode )
+
+    if not os.path.exists( out_yaml ):
+        print('Creating diff remark YAML files...')
+        subprocess.run( opt_diff  + '--output ' + out_yaml + ' %s/%s.opt.yaml %s/%s.opt.yaml'%\
+                (report_dir, builds[0], report_dir, builds[1]), shell=True)
+    if not os.path.exists( out_html ):
+        print('Creating HTML report output...')
+        subprocess.run( './opt-viewer/opt-viewer.py --output-dir %s %s' %( out_html, out_yaml), shell=True)
+
+def generate_remark_reports( config, program ):
+    report_dir = './reports/' + program + '/'
+    combos = itertools.combinations( config[program]['build'], 2 )
+    for builds in combos:
+        diff_reports( report_dir, builds, 'all' )
+        diff_reports( report_dir, builds, 'analysis' )
+        diff_reports( report_dir, builds, 'missed' )
+        diff_reports( report_dir, builds, 'passed' )
+
+    print('Done generating compilation reports!')
+
+def fetch(config, program):
+    # directories
+    repo_dir = './repos/'
+    os.makedirs( repo_dir, exist_ok=True )
+
+    # clean and fetch (if needed)
+    subprocess.run( config[program]['fetch'], cwd=repo_dir, shell=True)
+
+def build(config, program):
+    fetch(config, program)
+    repo_dir = './repos/'
+
+    # build
+    for b in config[program]['build']:
+        compile_and_install(config, program, repo_dir, b)
+
+def main():
+    print('main')
+    parser = argparse.ArgumentParser(description='Benchmark and analyze programs compiled with different compilation options.')
+    parser.add_argument('-i', '--input', dest='input', type=str, help='configuration YAML input file for programs', required=True)
+    parser.add_argument('-f', '--fetch', dest='fetch', action='store_true', help='fetch program repos (without building)')
+    parser.add_argument('-b', '--build', dest='build', action='store_true', help='build programs (will fetch too)')
+    parser.add_argument('-r', '--run', dest='run', type=int, help='run <repetitions>')
+    parser.add_argument('-g', '--generate', dest='generate', action='store_true', help='generate compilation reports')
+    parser.add_argument('-p', '--programs', dest='programs', type=str, nargs='+', help='programs to run from the config')
+    parser.add_argument('-t', '--tags', dest='tags', type=str, nargs='+', help='tagged program to use from the config')
+    parser.add_argument('-s', '--stats', dest='stats', action='store_true', help='show run statistics')
+    parser.add_argument('-d', '--dry-run', dest='dry', action='store_true', help='enable dry run')
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as f:
+        config = yaml.load(f, Loader=CLoader)
+
+    print( '# apps: ', len(config) )
+    print('args.programs', args.programs)
+    print('args.tags', args.tags)
+    print('args.fetch', args.fetch)
+    print('args.build', args.build)
+    print('args.run', args.run)
+    print('args.generate', args.generate)
+
+    programs = []
+    if args.programs:
+        if args.programs == ['all']:
+            programs += config.keys()
+        else:
+            programs += args.programs
+    if args.tags:
+        for t in args.tags:
+            programs += [p for p in config.keys() if t in config[p]['tags'] and p not in programs]
+    for p in programs:
+        if args.fetch:
+            fetch( config, p )
+        if args.build:
+            build( config, p )
+        if args.run:
+            run( config, p, args.run, args.dry )
+        if args.generate:
+            generate_remark_reports( config, p )
+        if args.stats:
+            show_stats( config, p)
+
+if __name__ =='__main__':
+    main()
